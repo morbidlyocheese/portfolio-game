a7d79e2333a2bfde2881a33a0a03e754b417f340
django <damienjdarko@gmail.com>
2023-02-17 20:53:05 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import kaboom from 'kaboom';
+
+kaboom({
+    width: 320,
+    heigth: 240,
+    font: 'sinko',
+    background: [0, 0, 255],
+});
+
+add([
+    text('yello'),
+    pos(120, 80)
+]);
