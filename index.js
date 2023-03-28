a866842e5a6b28345d50f4aac560ed6ac6deab8d
django <damienjdarko@gmail.com>
2023-03-28 18:25:52 -0400

    :fire: basic game working

--- index.js
+++ index.js
@@ -7,55 +7,103 @@ kaboom({
     background: [0, 0, 255],
 });
 
-// setGravity(2400);
 loadSprite("js", "sprites/js.png");
 
 scene("main", () => {
+    let score = 0;
+    gravity(2400);
     layers(["bg","obj","ui"], "obj");
-});
+    ui = add([layer("ui")]);
+    const scoreLabel = ui.on('draw', () => {
+        drawText({
+            text: "Score: " + score,
+            size: 14,
+            font: "sink",
+            pos: vec2(8, 24)
+        });
+    });
+    ui.on("draw", () => {
+        drawText({
+            text: "Lives: " + js.lives,
+            size: 14,
+            font: "sink",
+            pos: vec2(8, 48)
+        });
+    });
+    
+    const platform = add([
+        rect(width(), 48),
+        pos(0, height() - 48),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
+    ]);
+    
+    function spawnObstacle() {
+        const obstacle = add([
+            rect(48, 64),
+            area(),
+            outline(4),
+            pos(width(), height() - 48),
+            origin('botleft'),
+            color(255, 180, 255),
+            move(LEFT, 240),
+            "obstacle"
+        ]);
+        wait(rand(0.5, 1.5), spawnObstacle);
+    }
+    
+    spawnObstacle();
+    
+    const js = add([
+        sprite("js"),
+        pos(80, 80),
+        area(),
+        body(),
+        rotate(0),
+        solid(),
+        origin("center"),
+        "player",
+        "mobile",
+        "wraps",
+        {
+            speed: 320,
+            lives: 3,
+            can_shoot: true,
+            special_cooldown: 0.5,
+            invulnerable: false,
+            invulnerablity_time: 3,
+            animation_frame: 0,
+        },
+    ]);
+    
+    js.onCollide('obstacle', () => {
+        addKaboom(js.pos);
+        shake();
+        js.lives--;
+    });
+    
+    onKeyDown("left", () => {
+        js.move(-js.speed, 0)
+    });
+    onKeyDown("right", () => {
+        js.move(js.speed, 0)
+    });
+    onKeyDown("space", () => {
+        if (js.isGrounded()) {
+            js.jump();
+        }
+    });
+    
+    onKeyDown('escape', () => {
+        go('main');
+    });
 
-let score = 0;
+    onUpdate(() => {
+        score++;
 
-ui = add([layer("ui")]);
-ui.on("draw", () => {
-    drawText({
-        text: "Score: " + score,
-        size: 14,
-        font: "sink",
-        pos: vec2(8, 24)
     });
 });
 
-const js = add([
-    sprite("js"),
-    pos(80, 80),
-    area(),
-    rotate(0),
-    solid(),
-    origin("center"),
-    "player",
-    "mobile",
-    "wraps",
-    {
-        speed: 320,
-        lives: 3,
-        can_shoot: true,
-        special_cooldown: 0.5,
-        invulnerable: false,
-        invulnerablity_time: 3,
-        animation_frame: 0,
-    },
-]);
-
-onKeyDown("left", () => {
-    js.move(-js.speed, 0)
-});
-onKeyDown("right", () => {
-    js.move(js.speed, 0)
-});
-onKeyDown("up", () => {
-    js.move(0, -js.speed)
-});
-onKeyDown("down", () => {
-    js.move(0, js.speed)
-});
+go('main');
--- readme.md
+++ readme.md
@@ -1,19 +1,22 @@
 [portfolio game](https://djangothesolarboy.github.io/portfolio-game/index.html)
 
+controls
+---
+- movement: left/right arrows
+- jump: space
+- restart: escape
+
 tools/technologies used
 ---
-
 - [kaboomjs](https://3000.kaboomjs.com/)
     - [tutorial reference](https://docs.replit.com/tutorials/kaboom/build-asteroids-with-kaboom)
 
 debugging commands
 ---
-
 - use `npm run build` periodically to have page update in realtime
 
 instructions
 ---
-
 1. clone repo
 2. unzip repo
 3. run `npm i` to install the dependencies
